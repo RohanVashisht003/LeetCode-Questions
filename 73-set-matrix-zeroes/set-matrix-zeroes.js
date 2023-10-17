81c40b8d8bc6e50d4bf19306d4a7593397b3172b
Rohan Sharma <38130791+RohanVashisht003@users.noreply.github.com>
2023-10-17 10:06:09 +0530

    Time: 74 ms (35.73%) | Memory: 45.3 MB (26.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -4,39 +4,28 @@
  */
 var setZeroes = function(matrix) {
     
-   let n = matrix.length;
-    let m = matrix[0].length;
-    let MIN = Number.MIN_VALUE;
+   let n = matrix.length
+   let m = matrix[0].length
+   
+   let row = new Array(n).fill(0)
+   let col = new Array(m).fill(0)
+   
+   for(let i=0; i<n; i++){
+       for(let j=0; j<m; j++){
+           if(matrix[i][j] === 0){
+              row[i] = 1
+               col[j] = 1
+           }
+       }
+   }
     
     for(let i=0; i<n; i++){
         for(let j=0; j<m; j++){
-            if(matrix[i][j]===0){
-                markRow(i,matrix,n,m,MIN)
-                markCol(j,matrix,n,m,MIN)
+            if(row[i] || col[j]){
+                matrix[i][j] = 0;
             }
         }
     }
-    for(let i=0; i<n; i++){
-        for(let j=0; j<m; j++){
-            if(matrix[i][j]===MIN){
-                matrix[i][j]=0;
-            }
-        }
-    }
-    return matrix;
+    return matrix
 };
 
-var markRow = function(i,matrix,n,m,MIN){
-    for(let j=0; j<m; j++){
-        if(matrix[i][j]!==0){
-            matrix[i][j]=MIN
-        }
-    }
-}
-var markCol = function(j,matrix,n,m,MIN){
-    for(let i=0; i<n; i++){
-        if(matrix[i][j]!==0){
-            matrix[i][j]=MIN
-        }
-    }
-}
