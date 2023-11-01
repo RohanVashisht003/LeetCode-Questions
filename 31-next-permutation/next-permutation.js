2ff96ff84932cf28c7f534dd6295b11e3c20e62f
Rohan Sharma <38130791+RohanVashisht003@users.noreply.github.com>
2023-11-01 22:51:01 +0530

    Time: 63 ms (30.22%) | Memory: 43.6 MB (36.22%) - LeetSync

--- 31-next-permutation/next-permutation.js
+++ 31-next-permutation/next-permutation.js
@@ -3,26 +3,28 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    let index = -1;
-    let n = nums.length;
+    // step 1 find breakpoint
+    let n = nums.length
+    let index=-1;
     for(let i=n-2; i>=0; i--){
         if(nums[i]<nums[i+1]){
-            index = i
-            break
-        }
-    }
-    
-    if(index===-1){
-        nums.reverse();
-        return nums;
-    }
-    
-    for(let i=n-1; i>=index; i--){
-        if(nums[i]>nums[index]){
-            [nums[i],nums[index]] = [nums[index],nums[i]];
+            index = i;
             break;
         }
     }
-    nums.splice(index+1,n-index-1,...nums.slice(index+1).reverse())
-    return nums
+
+    if(index==-1){
+        nums.reverse();
+        return nums;
+    }
+
+    for(let i=n-1; i>index; i--){
+        if(nums[i]>nums[index]){
+            [nums[i],nums[index]] = [nums[index],nums[i]]
+            break;
+        }
+    }
+    nums.splice(index+1,nums.length-index-1,...nums.slice(index+1).reverse())
+    return nums;
+
 };
