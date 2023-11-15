952c454aa870402cfb3dc3e418af7c705b052872
Rohan Sharma <38130791+RohanVashisht003@users.noreply.github.com>
2023-11-15 12:01:49 +0530

    Time: 88 ms (71.74%) | Memory: 48.7 MB (77.12%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -5,25 +5,38 @@
 var merge = function(intervals) {
     let n = intervals.length;
     intervals.sort((a,b)=>a[0]-b[0])
-    const ans=[]
+//     const ans=[]
     
-    for(let i=0; i<n; i++){
-        let start = intervals[i][0];
-        let end = intervals[i][1];
+//     for(let i=0; i<n; i++){
+//         let start = intervals[i][0];
+//         let end = intervals[i][1];
         
-        if(ans.length && end<=ans[ans.length-1][1]){
-            continue;
-        }
+//         if(ans.length && end<=ans[ans.length-1][1]){
+//             continue;
+//         }
         
-        for(let j=i+1; j<n; j++){
-            if(intervals[j][0]<=end){
-                end=Math.max(end,intervals[j][1])
-            }
-            else{
-                break;
-            }
+//         for(let j=i+1; j<n; j++){
+//             if(intervals[j][0]<=end){
+//                 end=Math.max(end,intervals[j][1])
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         ans.push([start,end])
+//     }
+    const ans = [intervals[0]];
+    
+    for(let i=1; i<n; i++){
+        const last = ans[ans.length-1];
+        const curr = intervals[i];
+        
+        if(curr[0]<=last[1]){
+            last[1] = Math.max(last[1],curr[1]);
         }
-        ans.push([start,end])
+        else{
+            ans.push(curr)
+        }
     }
     return ans;
 };
