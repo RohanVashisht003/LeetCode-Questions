b08ae38cb03b6239e3225e9e9c2584a9a933b6fc
Rohan Sharma <38130791+RohanVashisht003@users.noreply.github.com>
2023-11-28 11:24:12 +0530

    Time: 64 ms (92.72%) | Memory: 54.3 MB (36.45%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,10 +3,19 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    nums.sort()
+    let n = nums.length
+    let freq = []
+for(let i=0; i<=n+1; i++){
+    freq.push(0)
+}
+    
     for(let i=0; i<nums.length; i++){
-        if(nums[i]==nums[i+1]){
+        if(freq[nums[i]]==0){
+            freq[nums[i]]+=1
+        }
+        else{
             return nums[i]
         }
     }
+    return 0
 };
