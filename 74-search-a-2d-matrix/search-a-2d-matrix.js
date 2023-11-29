79ef76c876aedddafc38f0d406ccbcc24dca53b5
Rohan Sharma <38130791+RohanVashisht003@users.noreply.github.com>
2023-11-29 22:17:50 +0530

    Time: 56 ms (34.01%) | Memory: 42.2 MB (34.92%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -3,12 +3,24 @@
  * @param {number} target
  * @return {boolean}
  */
+
+var binarySearch = function(nums,target){
+    let low=0;
+    let high = nums.length-1;
+    
+    while(low<=high){
+        let mid = Math.floor((low+high)/2)
+         
+        if (nums[mid] === target) return true;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false
+}
 var searchMatrix = function(matrix, target) {
     for(let i=0; i<matrix.length; i++){
-        for(let j=0; j<matrix[0].length; j++){
-          if(matrix[i][j]=== target){
-              return true;
-          }  
+        if(matrix[i][0]<=target && target<= matrix[i][matrix[0].length-1]){
+            return binarySearch(matrix[i],target);
         }
     }
     return false
